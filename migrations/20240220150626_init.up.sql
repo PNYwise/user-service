207318d927bb0154c34439d26f33583a7919be69
pnywise <bunayya12345@gmail.com>
2024-02-21 20:21:27 +0700

    update query migration

--- migrations/20240220150626_init.up.sql
+++ migrations/20240220150626_init.up.sql
@@ -3,15 +3,16 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY, 
     uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    email VARCHAR NOT NULL,
+    email VARCHAR,
     username VARCHAR(20) NOT NULL,
     timezone VARCHAR,
-    follower BIGINT DEFAULT 0;
-    following BIGINT DEFAULT 0;
+    followers BIGINT DEFAULT 0,
+    following BIGINT DEFAULT 0,
     created_at timestamp,
     updated_at timestamp,
     deleted_at timestamp,
-    CONSTRAINT unique_users_username UNIQUE (username)
+    CONSTRAINT unique_users_username UNIQUE (username),
+    CONSTRAINT unique_users_email UNIQUE (email) WHERE email IS NOT NULL
 );
 CREATE INDEX IF NOT EXISTS idx_users_deleted_at ON users (deleted_at) WHERE deleted_at IS NOT NULL;
 CREATE INDEX IF NOT EXISTS idx_users_created_at ON users (created_at) WHERE created_at IS NOT NULL;
@@ -21,7 +22,8 @@ CREATE TABLE IF NOT EXISTS followers (
     followed_id BIGINT NOT NULL,
     following_id BIGINT NOT NULL,
     created_at timestamp,
-    CONSTRAINT fk_followed_user_id FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE,
-    CONSTRAINT fk_following_user_id FOREIGN KEY (following_id) REFERENCES users(id) ON DELETE CASCADE
+    CONSTRAINT fk_followed_user_id FOREIGN KEY (followed_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_following_user_id FOREIGN KEY (following_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT unique_followed_following_id UNIQUE (followed_id,following_id),
 );
 CREATE INDEX IF NOT EXISTS idx_created_at ON followers (created_at) WHERE created_at IS NOT NULL;

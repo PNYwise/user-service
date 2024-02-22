c8b90145e52378a69c3e8750a6f33dd2d71c056f
pnywise <bunayya12345@gmail.com>
2024-02-22 22:07:57 +0700

    add user service

---
+++ internal/service/user_service.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"context"
+	"errors"
+
+	"github.com/PNYwise/user-service/internal/domain"
+)
+
+type userService struct {
+	userRepo domain.IUserRepository
+}
+
+func NewUserService(userRepo domain.IUserRepository) domain.IUserService {
+	return &userService{userRepo}
+}
+
+// Create implements domain.IUserService.
+func (u *userService) Create(ctx context.Context, request *domain.CreateUserRequest) (*domain.User, error) {
+	user := &domain.User{
+		Email:    request.Email,
+		Username: request.Username,
+		Timezone: request.Timezone,
+	}
+	if err := u.userRepo.Create(ctx, user); err != nil {
+		return nil, errors.New("internal server error")
+	}
+	return user, nil
+}
+
+// GetByUuid implements domain.IUserService.
+func (*userService) GetByUuid(ctx context.Context, uuid string) (*domain.User, error) {
+	panic("unimplemented")
+}
